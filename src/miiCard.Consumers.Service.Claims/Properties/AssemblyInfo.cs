f6ca4223516e48b42c22b611d7708a9328516081
Paul O'Neill <Paul.ONeill@miicard.com>
2013-09-05 14:12:59 +0100

    Fix missing assy description

--- src/miiCard.Consumers.Service.Claims/Properties/AssemblyInfo.cs
+++ src/miiCard.Consumers.Service.Claims/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("miiCard.Consumers.Service.Claims")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET wrapper classes for the miiCard web API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("miiCard Limited")]
 [assembly: AssemblyProduct("miiCard.Consumers.Service.Claims")]
